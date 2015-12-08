804eee9041e2e03f665fd83ac05c47ddf84c728b
MPVillafranca <mpvillafranca@correo.ugr.es>
2015-12-08 16:51:05 +0100

    static

--- hearcloud/urls.py
+++ hearcloud/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 admin.autodiscover()
 
 urlpatterns = [
@@ -26,3 +27,5 @@ urlpatterns = [
 
 	url(r'^admin/', include(admin.site.urls)),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
