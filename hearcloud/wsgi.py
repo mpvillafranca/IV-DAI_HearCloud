4965cf2f277a0dcd778bf95e4cad17ec95d48291
MPVillafranca <mpvillafranca@correo.ugr.es>
2015-12-08 03:16:57 +0100

    staticfiles en heroku

--- hearcloud/wsgi.py
+++ hearcloud/wsgi.py
@@ -6,17 +6,31 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
-
 import os
 
+LOCAL_DEVELOPMENT = True
+HEROKU_ENVIRONMENT, TRAVIS_ENVIRONMENT, SNAP_CI_ENVIRONMENT = False, False, False
+
+if 'HEROKU_ENVIRONMENT' in os.environ:
+    LOCAL_DEVELOPMENT = False
+    HEROKU_ENVIRONMENT = True
+elif 'TRAVIS' in os.environ:
+    LOCAL_DEVELOPMENT = False
+    TRAVIS_ENVIRONMENT = True
+elif 'SNAP_CI' in os.environ:
+    LOCAL_DEVELOPMENT = False
+    SNAP_CI_ENVIRONMENT = True
+
+if not HEROKU_ENVIRONMENT:
+    env = os.environ
+
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
 
-ON_HEROKU = os.environ.get('PORT')
-if ON_HEROKU:
-	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hearcloud.settings.staging")
-	application = Cling(get_wsgi_application())
-else:
+if LOCAL_DEVELOPMENT:
 	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hearcloud.settings.local")
-	application = get_wsgi_application()
+else:
+	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hearcloud.settings.staging")
 
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
